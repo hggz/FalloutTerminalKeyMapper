00288fd59cd32cc619b2dd3755ed18f7395a3d59
Hugo Artemio Gonzalez <hagonzalez94@gmail.com>
2015-02-28 10:40:56 -0800

    Initial Commit

---
+++ FalloutTerminalKeyMapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FalloutTerminalKeyMapper
+//
+//  Created by Artemis on 2/28/15.
+//  Copyright (c) 2015 mdt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FalloutTerminalKeyMapper/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FalloutTerminalKeyMapper
+//
+//  Created by Artemis on 2/28/15.
+//  Copyright (c) 2015 mdt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
